69ea2159de475a8a980df42aff76a38c1e7fecdf
Kartik522001 <panchalkartik30@gmail.com>
2022-02-10 22:13:09 +0530

    main

---
+++ client/src/components/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <p>Welcome</p>
+        <h1>We are the mern Developer</h1>
+    </div>
+  )
+}
+
+export default Signup
